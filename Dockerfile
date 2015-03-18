6ed0185799f235080d6bb7dc485a2b14ca7cb15a
tom <tom@skibinski.it>
2015-03-18 17:10:11 +0100

    nginx config modyfication

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,10 @@ FROM nginx
 EXPOSE 80 443
 
 COPY nginx.conf /etc/nginx/
-COPY default.conf /etc/nginx/conf.d/
 
 ENV NGINX_RUN_USER nginx
 ENV NGINX_LOG_DIR /var/log/nginx
 ENV NGINX_BACKEND_IP 127.0.0.1
-ENV NGINX_BACKEND_PORT 8080
-ENV NGINX_LISTENER_PORT 8000
 
 ADD nginx.sh /nginx.sh
 RUN chmod +x /nginx.sh
--- nginx.sh
+++ nginx.sh
@@ -4,10 +4,5 @@
 [ -z "${NGINX_RUN_USER}" ] && echo "\$NGINX_RUN_USER is not set" || sed -i "s/NGINX_RUN_USER/${NGINX_RUN_USER}/" /etc/nginx/nginx.conf
 [ -z "${NGINX_LOG_DIR}" ] && echo "\$NGINX_LOG_DIR is not set" || sed -i "s#NGINX_LOG_DIR#${NGINX_LOG_DIR}#" /etc/nginx/nginx.conf
 
-[ -z "${NGINX_BACKEND_IP}" ] && echo "\$NGINX_BACKEND_IP is not set" || sed -i "s/NGINX_BACKEND_IP/${NGINX_BACKEND_IP}/" /etc/nginx/conf.d/default.conf
-[ -z "${NGINX_BACKEND_PORT}" ] && echo "\$NGINX_BACKEND_PORT is not set" || sed -i "s/NGINX_BACKEND_PORT/${NGINX_BACKEND_PORT}/" /etc/nginx/conf.d/default.conf
-[ -z "${NGINX_SERVER_NAME}" ] && echo "\$NGINX_SERVER_NAME is not set" || sed -i "s/#server_name NGINX_SERVER_NAME;/server_name ${NGINX_SERVER_NAME};/" /etc/nginx/conf.d/default.conf
-[ -z "${NGINX_LOG_DIR}" ] && echo "\$NGINX_LOG_DIR is not set" || sed -i "s#NGINX_LOG_DIR#${NGINX_LOG_DIR}#" /etc/nginx/conf.d/default.conf
-
 # Start nginx
 /etc/init.d/nginx start
